07bee02eca941c78fbf65245748bfd2c1715a3e5
indhumathipalanisamy <39158570+indhumathipalanisamy@users.noreply.github.com>
2019-08-14 04:29:55 +0530

    Update multiple.c

--- multiple.c
+++ multiple.c
@@ -2,13 +2,13 @@
 #include<conio.h>
 void main()
 {
-int sum=0,i,n;
+int i,n;
 clrscr();
-printf("\n enter the n number");
+printf("\n enter the  number");
 scanf("%d",&n);
-for(i=1;i<=n;i++)
+for(i=1;i<=5;i++)
 {
-printf(5*i);
+printf(n*i);
 }
 getch();
 }
