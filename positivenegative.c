f90988f72a8dbcae9bd6f7547acba405839b06f4
indhumathipalanisamy <39158570+indhumathipalanisamy@users.noreply.github.com>
2019-08-13 21:02:51 +0530

    Create positivenegative.c

---
+++ positivenegative.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+printf("\n enter the number");
+scanf("%d",&a);
+if(a>0)
+printf("\n positive");
+else if(a<0)
+printf("\n negative");
+else if(a==0)
+printf("\n zero");
+getch();
+}
