dbe3df374c51f60fa9590db6185596e0bef69794
indhumathipalanisamy <39158570+indhumathipalanisamy@users.noreply.github.com>
2019-08-14 04:26:20 +0530

    Create multiple.c

---
+++ multiple.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int sum=0,i,n;
+clrscr();
+printf("\n enter the n number");
+scanf("%d",&n);
+for(i=1;i<=n;i++)
+{
+printf(5*i);
+}
+getch();
+}
